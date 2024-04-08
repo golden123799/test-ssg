a72e72a07ffd7ae80bbda40d891f3cb32b60b661
Golden Fam Fook Cheng <fookcheng@9loop.asia>
2024-04-08 11:09:46 +0800

    edit this page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
     console.log('hello world');
 </script>
 
-<h1>Hello world this is 1105</h1>
+<h1>Hello world this is 1105 this is edit</h1>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
